382245daf04ada579335452abf642be7f966c0ea
Simao Ferreira <24299428+simao-ferreira@users.noreply.github.com>
2022-05-22 17:44:17 +0200

    CV: Add script for creating pdf from markdown resume

---
+++ build_pdf_resume.py
@@ -0,0 +1,29 @@
+import datetime
+import sys
+
+from md2pdf.core import md2pdf
+
+
+def set_appearance():
+    css_file_input = f'styles/simple-style.css'
+    if len(sys.argv) > 2:
+        sys.exit(f'Unexpected arguments found, {len(sys.argv)} arguments were found, 1 was expected.')
+    elif len(sys.argv) == 2:
+        css_file_input = sys.argv[1]
+    return css_file_input
+
+
+def build_pdf_filepath():
+    timestamp = datetime.date.today()
+    return f'output/resume-{timestamp}.pdf'
+
+
+css_file_path = set_appearance()
+pdf_file_path = build_pdf_filepath()
+resume_path = 'RESUME.md'
+
+md2pdf(pdf_file_path,
+       md_content=None,
+       md_file_path=resume_path,
+       css_file_path=css_file_path,
+       base_url=None)
